0b5e6ad4099f1a96560932ef76a18f7281061f25
Colin Campbell <colin.campbell@payroc.com>
2022-01-21 11:49:24 +0000

    Allow book create via URL submission

--- lib/MyApp/Controller/Books.pm
+++ lib/MyApp/Controller/Books.pm
@@ -26,6 +26,22 @@ sub index : Path : Args(0) {
 	$c->response->body('Matched MyApp::Controller::Books in Books.');
 }
 
+=head2 base
+ 
+Can place common logic to start chained dispatch here
+ 
+=cut
+
+sub base : Chained('/') : PathPart('books') : CaptureArgs(0) {
+	my ($self, $c) = @_;
+
+	# Store the ResultSet in stash so it's available for other methods
+	$c->stash(resultset => $c->model('DB::Book'));
+
+	# Print a message to the debug log
+	$c->log->debug('*** INSIDE BASE METHOD ***');
+}
+
 =head2 list
  
 Fetch all book objects and pass to books/list.tt2 in stash to be displayed
@@ -50,6 +66,46 @@ sub list : Local {
 	# in your action methods (action methods respond to user input in
 	# your controllers).
 	$c->stash(template => 'books/list.tt2');
+}
+
+=head2 url_create
+ 
+Create a book with the supplied title, rating, and author
+ 
+=cut
+
+sub url_create : Chained('base') : PathPart('url_create') : Args(3) {
+
+	# In addition to self & context, get the title, rating, &
+	# author_id args from the URL.  Note that Catalyst automatically
+	# puts extra information after the "/<controller_name>/<action_name/"
+	# into @_.  The args are separated  by the '/' char on the URL.
+	my ($self, $c, $title, $rating, $author_id) = @_;
+
+	# Call create() on the book model object. Pass the table
+	# columns/field values we want to set as hash values
+	my $book = $c->model('DB::Book')->create(
+		{
+			title => $title,
+			rating => $rating
+		}
+	);
+
+	# Add a record to the join table for this book, mapping to
+	# appropriate author
+	$book->add_to_book_authors({ author_id => $author_id });
+
+	# Note: Above is a shortcut for this:
+	# $book->create_related('book_authors', {author_id => $author_id});
+
+	# Assign the Book object to the stash for display and set template
+	$c->stash(
+		book => $book,
+		template => 'books/create_done.tt2'
+	);
+
+	# Disable caching for this page
+	$c->response->header('Cache-Control' => 'no-cache');
 }
 
 =encoding utf8
